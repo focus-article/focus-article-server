6932112300aeed7fc2594ca4e096c2e50dc7740d
mathiasgheno <mathiasgheno@gmail.com>
2025-09-23 03:25:57 -0300

    chore: create infra/database if does not exists

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,23 @@
 import express from "express";
 import { Database } from "bun:sqlite";
+import { existsSync, mkdirSync } from "fs";
+import path from "path";
 import configDatabase from "./config-database.ts";
 import configServer from "./config-server.ts";
 
-const db = new Database("./infra/database.db");
+const dbPath = "./infra/database.db";
+
+const dirPath = path.dirname(dbPath);
+if (!existsSync(dirPath)) {
+  mkdirSync(dirPath, { recursive: true });
+}
+const db = new Database(dbPath);
+
 const app = express();
 
 configDatabase(db);
 configServer(app, db);
 
 app.listen(3001, () => {
-  console.log("🚀 API running at http://localhost:3000");
+  console.log("🚀 API running at http://localhost:3002");
 });
